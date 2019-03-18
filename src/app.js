ee78f0e10f91ffb6cd0ff71f0eccb2410c5f53b7
kadupenido <contato@kadupenido.com>
2019-03-18 18:09:12 -0300

    commit inicial

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const config = require('./config/config');
+
+// Instância do app
+const app = express();
+
+// Habilita o log
+app.use(morgan('dev'));
+
+// Habilita cors
+app.use(cors());
+
+// Configura o body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Carrega o mongoose
+// mongoose.connect(config.database);
+
+// Carrega as rotas
+const mainRoute = require('./app-router');
+
+app.use('/', mainRoute);
+
+module.exports = app;
---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+const config = {
+    port: 3000,
+    database: 'mongodb://localhost/eleitor'
+}
+
+module.exports = config;
