56a6113ec94401bc186dae4367489875b157f82b
Carlos Eduardo Campos Penido <contato@kadupenido.com>
2019-05-16 10:58:46 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 const config = {
     port: 3000,
-    database: 'mongodb://root:e02b0add03@ds225902.mlab.com:25902/eleitoresdb',
-    PW_KEY: 'E60F725EA6214B809CE27B50AAE221B1',
-    PRIVATE_KEY: '6A5EF55B026E4236B967568D343E0D67'
+    database: '',
+    PW_KEY: '',
+    PRIVATE_KEY: ''
 }
 
 module.exports = config;
